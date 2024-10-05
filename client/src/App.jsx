c90afd592e6a16c53b21864439a8e4771dc9fc0e
Ivan Mijares <ivan.f.mijares@gmail.com>
2024-10-05 16:34:20 -0700

    Ultima versio, ver si estamos iguales

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     <div className="App">
       <Main />
       <p>{data}</p>
-      <h1 className='text-9xl'>NIGGA</h1>
+      <h1 className='text-9xl'>Hello World actualizado</h1>
     </div>
   );
 }
