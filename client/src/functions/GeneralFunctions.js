f750069f196d706baad818a008cff07526ccd257
Ivan Mijares <ivan.f.mijares@gmail.com>
2024-10-06 02:45:51 -0700

    Funciones para el Connection

--- client/src/functions/GeneralFunctions.js
+++ client/src/functions/GeneralFunctions.js
@@ -1,36 +1,29 @@
+    const getTopics = () => { 
+        const mySet = new Set();
+        const topics=["Fin de la pobreza","Hambre cero","Salud y bienestar","Educación de calidad", "Igualdad de género", "Agua limpia y saneamiento","Trabajo decente y crecimiento económico","Industria, innovación e infraestructura","Reducción de las desigualdades","Ciudades y comunidades sostenibles","Producción y consumo responsables","Acción por el clima","Vida submarina","Vida de ecosistemas terrestres","Paz, justicia e instituciones sólidas","Alianzas para lograr los objetivos"]
 
-export const getTopics = (arr) => { 
-
-    const mySet = new Set();
-
-    while(mySet.size < 4){
-
-        const randomIndex = Math.floor(Math.random() * arr.length);
-
-        if(!mySet.has(arr)){
-            mySet.add(arr[randomIndex]);
-
+        while(mySet.size < 4){
+            const randomIndex = Math.floor(Math.random() * topics.length);
+            if(!mySet.has(topics[randomIndex]))
+            {
+                mySet.add(topics[randomIndex]);
+            }
         }
-
-    }
-    
-    
-    const myNewArray = Array.from(mySet);
-    console.log(myNewArray);
-    return myNewArray;
-   
-}
-
-
-export const selectWords = (topics) => {
-
-
-    for(let i = 0; i< topics.length ; i++){
-
-        const randomIndex = Math.floor(Math.random() * topics.length);
-
-
-
+        const myNewArray = Array.from(mySet);
+        console.log(myNewArray);
+        return myNewArray;
     }
 
-}
+    export const selectWords = (conectorODS) => 
+    {
+        let arr;
+        let topics=getTopics();
+        for(let i = 0; i< topics.length ; i++)
+        {
+            const randomIndex = Math.floor(Math.random() * 3);
+            let curr=conectorODS[topics[i]];
+            arr.push(curr[randomIndex]);
+        }
+        console.log(arr);
+        return arr;
+    }
