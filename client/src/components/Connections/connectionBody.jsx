57a30aa1f3906ed40cf1396d08798cfbf3a8a4a5
Emiliano0X1 <egp2506@gmail.com>
2024-10-06 03:01:38 -0700

    QuiTE

--- client/src/components/Connections/connectionBody.jsx
+++ client/src/components/Connections/connectionBody.jsx
@@ -1,14 +1,11 @@
 
 import { Button, SimpleGrid} from "@chakra-ui/react";
-import { getTopics } from "../../functions/GeneralFunctions";
 
 import { connectorODS } from "../../utils/conectorODS";
 
 
 export default function BodyConnection() {
 
-    const result = getTopics(connectorODS);
-    console.log(result)
     return (
       <SimpleGrid columns={4} spacing={4} justifyContent={'center'} alignItems="center" p={[2, 7, 10]}>
         {Array.from({ length: 16 }, (_, index) => {
